3dee1d8f2638f7a3bac965d661a7f5cb3fe7c478
ThiagoGwebg <thiagoribeiro18181@gmail.com>
2026-03-13 20:28:29 -0300

    feat: verify number

--- imc/js/script.js
+++ imc/js/script.js
@@ -3,15 +3,18 @@ const inputPeso = document.getElementById("peso");
 const inputAltura = document.getElementById("altura");
 const divResultado = document.getElementById("resultado");
 
-form.addEventListener("submit", function(evento){
-//esse comando evita que o formulario seja atualizado
+form.addEventListener("submit", function (evento) {
+    //esse comando evita que o formulario seja atualizado
     evento.preventDefault();
-    
-//pega os valores digitados no input
+
+    //pega os valores digitados no input
     const peso = parseFloat(inputPeso.value);
     const altura = parseFloat(inputAltura.value);
-   
-//validar se é realmente numero
-    
-    
+
+    //validar se é realmente numero
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+
+    }
+
+
 });
