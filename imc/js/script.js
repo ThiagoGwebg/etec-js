8841f38ff0152be960d4ab90bfac3e5f45b2f0d5
ThiagoGwebg <thiagoribeiro18181@gmail.com>
2026-03-13 20:23:46 -0300

    feat: Imc calculator

---
+++ imc/js/script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("imc-form");
+const inputPeso = document.getElementById("peso");
+const inputAltura = document.getElementById("altura");
+const divResultado = document.getElementById("resultado");
+
+form.addEventListener("submit", function(evento){
+//esse comando evita que o formulario seja atualizado
+    evento.preventDefault();
+    
+//pega os valores digitados no input
+    const peso = parseFloat(inputPeso.value);
+    const altura = parseFloat(inputAltura.value);
+   
+//validar se é realmente numero
+    
+    
+});
